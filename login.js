89103adba426fb3ce2f51428848f25b0794ac46c
YashThakare6525 <thakareyash238@gmail.com>
2024-08-16 06:59:47 +0530

    added website pages

---
+++ login.js
@@ -0,0 +1,16 @@
+{let header = document.querySelector('header');
+
+header.classList.toggle('sticky',window.scrollY > 100);
+
+};
+
+
+{ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+ });
+
+ ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+}
